634d87d339a7c5e40f1a85b381a773a94ff88479
Patrick Teles <patrickchavesteles@gmail.com>
2024-07-27 17:24:15 -0300

    Add files via upload

---
+++ Estudo de programação - Tesla/scrips.js
@@ -0,0 +1,5 @@
+let formulario = document.querySelector(".formulario")
+function aparecerFormulario() {
+    formulario.style.left = "28.5%"}
+function desaparecerFormulario() {
+    formulario.style.left = "-50%"}
